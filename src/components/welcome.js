d76dd7e581bb3dcd7f75c834db229ad26f367541
Ashkan Rahmani <ashkan.rmk@hotmail.com>
2018-11-30 18:38:34 +0330

    Add gender selection

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 import logo from '../dist/img/logo.svg';
 import '../dist/css/index.css';
 import 'antd/dist/antd.css';
-import { Button } from 'antd';
+import { Button, Icon, Divider, Radio } from 'antd';
+
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
 
 class Start extends Component {
 	render() {
@@ -12,12 +15,47 @@ class Start extends Component {
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 
-					<a className="App-link" href="https://ashkan-rmk.github.io/awesome-persian-podcasts/" target="_blank" rel="noopener noreferrer">
-						به فرم نظرسنجی پادکست‌های ایرانی خوش آمدید
-					</a>
-					<Link to="/start">
-						<Button>شروع !!</Button>
-					</Link>
+					<div style={{ direction: 'rtl', textAlign: 'center', padding: '20px' }}>
+						<p style={{ direction: 'rtl' }}>
+							ما اینجا سعی داریم تا اطلاعات لازم برای تگ‌بندی و آموزش یک سیستم پیشنهاددهنده برای پادکست‌های فارسی
+							جمع‌اوری کنیم. خوشحال میشیم تا یکم از وقتتون رو به ما بدین و این فرم رو تکمیل کنین. پیشاپیش ممنونیم ازتون
+							:)
+						</p>
+						{/* <Divider /> */}
+						<p>
+							برای مشاهده یک لیست کاملی از پادکست‌های فارسی هم میتونین به
+							<a
+								className="App-link"
+								href="https://ashkan-rmk.github.io/awesome-persian-podcasts/"
+								target="_blank"
+								rel="noopener noreferrer">
+								اینجا
+							</a>
+							یک سری بزنین
+						</p>
+						<p>لطفا یکی رو انتخاب کنین</p>
+
+						<RadioGroup defaultValue="man" size="large">
+							<RadioButton value="man">
+								{' '}
+								<Icon type="man" />
+								آقا
+							</RadioButton>
+							<RadioButton value="woman">
+								{' '}
+								<Icon type="woman" />
+								خانم
+							</RadioButton>
+						</RadioGroup>
+
+						<Divider dashed />
+						<Link to="/start">
+							<Button size="large" type="primary">
+								شروع
+								<Icon type="fire" />
+							</Button>
+						</Link>
+					</div>
 				</header>
 			</div>
 		);
